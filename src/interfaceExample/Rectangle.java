d27b28c3eb26c601932708815d20c9660375db8b
Jon Fincher <jfincher42@gmail.com>
2018-03-23 08:55:00 -0700

    Implementation of interfacew HW

--- src/interfaceExample/Rectangle.java
+++ src/interfaceExample/Rectangle.java
@@ -4,8 +4,23 @@ package interfaceExample;
 // and add a constructor that takes two parameters of type double, named length and width.
 // See the Circle class for an example of how to implement the Shape interface.
 
-public class Rectangle /* something important goes here */ {
+public class Rectangle implements Shape {
 	// field declarations go here
+	private double length;
+	private double width;
 	// constructor goes here
+	
+	public Rectangle(double length, double width) {
+		this.length = length;
+		this.width = width;
+	}
 	// methods of the Shape interface go here
+	
+	public double area() {
+		return this.length * this.width;
+	}
+	
+	public double perimeter() {
+		return 2 * (this.length + this.width);
+	}
 }
--- src/interfaceExample/ShapeExample.java
+++ src/interfaceExample/ShapeExample.java
@@ -10,17 +10,27 @@ public class ShapeExample {
 	}
 	
 	// Lab part 1A: add a new method getTotalPerimeter
+	public static double getTotalPerimeter(Shape[] shapes) {
+		double total = 0.0;
+		for (Shape shape : shapes) {
+			total += shape.perimeter();
+		}
+		return total;
+	}
 
 	public static void main(String[] args) {
 		Shape[] shapes = new Shape[] {
 				new Circle(1.0),
-				new Circle(2.0)
+				new Circle(2.0),
 				// Lab part 2B: add another element: a new Rectangle with length 2.0 and width 3.0
 				// Don't forget to add a comma after the previous element.
+				new Rectangle(2.0, 3.0)
 		};
 		double totalArea = getTotalArea(shapes);
 		System.out.println("Total area of all shapes is " + totalArea);
 		// Lab part 1B: calculate and print total perimeter 
+		double totalPerimeter = getTotalPerimeter(shapes);
+		System.out.println("Total perimeter of all shapes is " + totalPerimeter);
 	}
 
 }
